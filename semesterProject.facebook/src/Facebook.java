aaf15c34a3d34ad13cb53720efd875167de416c3
karim kohel <kareemkohel@gmail.com>
2021-06-01 16:35:54 +0200

    init

---
+++ semesterProject.facebook/src/Facebook.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Facebook {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(860,640);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+    }
+}
