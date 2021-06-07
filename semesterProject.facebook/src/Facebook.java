e3fbf90f12cae251f429695dc4d8deea70d142c2
karimkohel <kareemkohel@gmail.com>
2021-06-07 12:19:35 +0200

    added basic GUI to main

--- semesterProject.facebook/src/Facebook.java
+++ semesterProject.facebook/src/Facebook.java
@@ -3,10 +3,34 @@ import javax.swing.*;
 public class Facebook {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(860,640);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
         JPanel panel = new JPanel();
+        frame.setSize(460,240);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel usernameLabel = new JLabel("Username");
+        usernameLabel.setBounds(10,20,80,25);
+        panel.add(usernameLabel);
+
+        JTextField usernameTextField = new JTextField(20);
+        usernameTextField.setBounds(100,20,165,25);
+        panel.add(usernameTextField);
+
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordTextField = new JPasswordField(20);
+        passwordTextField.setBounds(100,50,165,25);
+        panel.add(passwordTextField);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+
+        frame.setVisible(true);
     }
 }
