e62e8f824947bd53f0e4b83a4d7ab1629a9b0131
karimkohel <kareemkohel@gmail.com>
2021-06-07 22:29:20 +0200

    added beautiful colors

--- src/packageFacebook/LoginFrame.java
+++ src/packageFacebook/LoginFrame.java
@@ -1,6 +1,7 @@
 package packageFacebook;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginFrame extends MainFrame implements ShowOutputInterface{
     private final JButton loginButton = new JButton("Login");
@@ -14,28 +15,39 @@ public class LoginFrame extends MainFrame implements ShowOutputInterface{
         JLabel passwordLabel = new JLabel("Password");
         JPasswordField passwordTextField = new JPasswordField(30);
 
-        usernameLabel.setBounds(600,200,80,25);
+        usernameLabel.setBounds(550,200,80,25);
+        usernameLabel.setForeground(new Color(0xFFFFFF));
         this.add(usernameLabel);
 
-        usernameTextField.setBounds(700,200,165,25);
+        usernameTextField.setBounds(650,200,165,25);
         this.add(usernameTextField);
 
-        passwordLabel.setBounds(600, 250, 80, 25);
+        passwordLabel.setBounds(550, 250, 80, 25);
+        passwordLabel.setForeground(new Color(0xFFFFFF));
         this.add(passwordLabel);
 
-        passwordTextField.setBounds(700,250,165,25);
+        passwordTextField.setBounds(650,250,165,25);
         this.add(passwordTextField);
 
-        this.successLabel.setBounds(10,110,300,25);
-        this.add(successLabel);
-
-        this.loginButton.setBounds(10, 80, 80, 25);
+        this.loginButton.setBounds(660, 300, 100, 25);
         this.loginButton.setFocusable(false);
         // lambda expressions are better than implementing interfaces
         this.loginButton.addActionListener(e -> this.loginButtonPress());
         this.add(loginButton);
 
+        this.registerButton.setBounds(660, 350, 100, 25);
+        this.registerButton.setFocusable(false);
+        this.registerButton.addActionListener(e -> this.registerButtonPress());
+        this.add(registerButton);
+
+        this.successLabel.setBounds(700,350,300,25);
+        this.add(successLabel);
+
         this.setVisible(true);
+    }
+
+    private void registerButtonPress() {
+        // TODO
     }
 
     private void loginButtonPress(){
--- src/packageFacebook/MainFrame.java
+++ src/packageFacebook/MainFrame.java
@@ -1,11 +1,13 @@
 package packageFacebook;
 
 import javax.swing.*;
+import java.awt.*;
 
 public abstract class MainFrame extends JFrame{
     public MainFrame() {
         this.setSize(1420, 800);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(new Color(0x123451));
         this.setLayout(null);
     }
 }
